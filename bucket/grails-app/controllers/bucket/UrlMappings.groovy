27f24edf538912e01e5d855e1a457e922aa0aea2
chris13524 <chris@hichris.com>
2018-02-12 21:48:21 -0500

    Added home page.

--- bucket/grails-app/controllers/bucket/UrlMappings.groovy
+++ bucket/grails-app/controllers/bucket/UrlMappings.groovy
@@ -2,6 +2,7 @@ package bucket
 
 class UrlMappings {
 	static mappings = {
+		"/"(view: "/home")
 		get "/$emailAddress"(controller: "email", action: "retrieve")
 		
 		"404"(view: "/clientError/notFound")
