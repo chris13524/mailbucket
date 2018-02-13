54d7951da59de63b4a335d82ff658557ce321044
chris13524 <chris@hichris.com>
2018-02-12 21:54:57 -0500

    Tweaks to error pages.

--- bucket/grails-app/controllers/bucket/UrlMappings.groovy
+++ bucket/grails-app/controllers/bucket/UrlMappings.groovy
@@ -6,5 +6,6 @@ class UrlMappings {
 		get "/$emailAddress"(controller: "email", action: "retrieve")
 		
 		"404"(view: "/clientError/notFound")
+		"500"(view: "/serverError/internalSeverError")
 	}
 }
