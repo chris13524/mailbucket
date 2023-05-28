8e748b120cfff9865bef07defe3ee5f983a75b83
Chris Smith <chris@smith.xyx>
2023-05-27 22:14:14 -0400

    feat: args to specify ports

---
+++ src/args.rs
@@ -0,0 +1,9 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+pub struct Args {
+    /// SocketAddrs to bind to. https://doc.rust-lang.org/stable/std/net/trait.ToSocketAddrs.html#tymethod.to_socket_addrs
+    #[arg(short, long, default_value = "localhost:2525")]
+    pub addrs: String,
+}
--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,11 @@ mod dispatch;
 mod handler;
 mod stream;
 mod transport;
+mod args;
 
+use args::Args;
+use clap::Parser;
 use handler::MailHandler;
-use log::info;
 use samotop::mail::{Builder, DebugService};
 use samotop::server::TcpServer;
 use samotop::smtp::{Esmtp, SmtpParser};
@@ -13,10 +15,12 @@ type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    let args = Args::parse();
+
     env_logger::init();
     let service = Builder + DebugService::default() + Esmtp.with(SmtpParser) + MailHandler::new()?;
 
-    TcpServer::on("localhost:2525".to_string())
+    TcpServer::on(args.addrs)
         .serve(service.build())
         .await
 }
